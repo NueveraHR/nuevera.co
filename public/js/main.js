6ea4ed294a9cf3602da126fe84175942176aa58f
wassimoo <12980387+wassimoo@users.noreply.github.com>
2021-10-15 22:47:13 +0300

    Change with public

--- public/js/main.js
+++ public/js/main.js
@@ -19,10 +19,10 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src = "assets/images/logo/logo-2.svg";
+      logo.src = "public/images/logo/logo-2.svg";
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "assets/images/logo/logo.svg";
+      logo.src = "public/images/logo/logo.svg";
     }
 
     // show or hide the back-top-top button
